da35998b91fde41d6a332cf867bd8197b57312e1
valb-mig <valb.mig@gmail.com>
2024-03-17 20:21:24 -0300

    :sparkles:(feat): Funcionalidade de login para o usuário

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Pages\{
+use App\Http\Controllers\Pages\ {
     Catalogo\CatalogoController,
     Filme\FilmeController
 };
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Auth\ {
+    LoginController,
+    RegisterController
+};
+
+// Auth
+
+Route::get('/login',[
+    LoginController::class, 'index'
+])->name('login');
+
+Route::post('/login',[
+    LoginController::class, 'login'
+]);
+
+Route::get('/register',[
+    RegisterController::class, 'index'
+])->name('register');
+
+Route::post('/register',[
+    RegisterController::class, 'store'
+]);
+
+// Web
 
 Route::get('/',[
     CatalogoController::class, 'index'
